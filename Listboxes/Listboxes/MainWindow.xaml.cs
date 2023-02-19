3e71be2f8e9f56f8a700090cf219e9f520093338
cofeek-codes <kormyshev11@mail.ru>
2023-02-19 21:56:23 +0500

    listboxes

---
+++ Listboxes/Listboxes/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Listboxes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            float width = Convert.ToSingle(width_tbx.Text);
+            float height = Convert.ToSingle(height_tbx.Text);
+
+            // calculated
+
+            float size_calculated = 0;
+            float price_calculated = 0;
+
+            // calculated
+
+            count_btn.Click += Count_btn_Click;
+
+            
+
+        }
+
+        private void Count_btn_Click(object sender, RoutedEventArgs e)
+        {
+            switch(material_cmb.SelectedIndex)
+            {
+                // plastic
+
+                case 0:
+
+
+
+                    break;
+ 
+                    // aluminum
+
+                case 1:
+
+                    break;
+            }
+        }
+    }
+}
---
+++ kelt-horoscope/kelt-horoscope/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace kelt_horoscope
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Dictionary<int, string> dates = new Dictionary<int, string>();
+
+            Dictionary<string, string> descripts = new Dictionary<string, string>();
+
+            
+
+
+            //data in files
+
+
+            foreach (string file in Directory.EnumerateFiles("data", "*.txt"))
+            {
+                descripts.Add(File.ReadLines(file).ToList()[0], File.ReadAllText(file));
+
+            }
+
+
+
+            //data in files
+
+
+
+
+
+
+
+
+            foreach (TextBlock item in dateList.Items)
+            {
+                item.AddHandler(UIElement.MouseDownEvent,
+   new MouseButtonEventHandler(ListItem_MouseClick), true);
+
+                dates.Add(dateList.Items.IndexOf(item), item.Text);
+
+            }
+
+            void ListItem_MouseClick(object sender, MouseEventArgs e)
+            {
+                TextBlock itemTextBlock = (TextBlock)sender;
+            
+                int pos = dateList.Items.IndexOf(itemTextBlock);
+                
+                type_tbl.Text = descripts.Keys.ToArray()[pos].ToString();
+                desc_tbl.Text = descripts.Values.ToArray()[pos].ToString();
+                  
+
+
+            }
+
+
+        }
+
+      
+    }
+}
